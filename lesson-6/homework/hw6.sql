4da452489722a99f49eddc7bb0df7ca6ac18fb32
Usmonbek2704 <u57592109@gmail.com>
2025-09-26 21:21:38 +0300

    Create hw6.sql

---
+++ lesson-6/homework/hw6.sql
@@ -0,0 +1,84 @@
+
+CREATE TABLE InputTbl (
+    col1 VARCHAR(10),
+    col2 VARCHAR(10)
+);
+    INSERT INTO InputTbl (col1, col2) VALUES 
+('a', 'b'),
+('a', 'b'),
+('b', 'a'),
+('c', 'd'),
+('c', 'd'),
+('m', 'n'),
+('n', 'm');
+ SELECT DISTINCT
+       CASE WHEN col1 < col2 THEN col1 ELSE col2 END AS col1,
+       CASE WHEN col1 < col2 THEN col2 ELSE col1 END AS col2
+FROM InputTbl;
+
+
+  select col1, col2
+ from InputTbl
+ group by col1, col2
+
+ CREATE TABLE TestMultipleZero (
+    A INT NULL,
+    B INT NULL,
+    C INT NULL,
+    D INT NULL
+);
+
+INSERT INTO TestMultipleZero(A,B,C,D)
+VALUES 
+    (0,0,0,1),
+    (0,0,1,0),
+    (0,1,0,0),
+    (1,0,0,0),
+    (0,0,0,0),
+    (1,1,1,0);
+select A,B,C,D
+from TestMultipleZero
+WHERE NOT (A=0 AND B=0 AND C=0 AND D=0)
+
+create table section1(id int, name varchar(20))
+insert into section1 values (1, 'Been'),
+       (2, 'Roma'),
+       (3, 'Steven'),
+       (4, 'Paulo'),
+       (5, 'Genryh'),
+       (6, 'Bruno'),
+       (7, 'Fred'),
+       (8, 'Andro')
+
+SELECT *
+from section1
+where id%2=1
+
+SELECT min(id) as minid
+from section1
+
+select max(id) as maxid
+from section1
+
+select * 
+from section1
+where  name like 'B%'
+
+
+CREATE TABLE ProductCodes (
+    Code VARCHAR(20)
+);
+
+INSERT INTO ProductCodes (Code) VALUES
+('X-123'),
+('X_456'),
+('X#789'),
+('X-001'),
+('X%202'),
+('X_ABC'),
+('X#DEF'),
+('X-999');
+
+select * 
+from ProductCodes
+where Code like '%!_%' escape '!'
